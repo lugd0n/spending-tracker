aee336bac0c8c5777f219a4f95616074dfe3b9ad
lugd0n <daniel.luge@hotmail.com>
2022-06-16 13:52:56 +0200

    adds material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SpendingListComponent } from './spending-list/spending-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 
 @NgModule({
@@ -14,6 +16,8 @@ import { SpendingListComponent } from './spending-list/spending-list.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
   ],
   
   providers: [],
